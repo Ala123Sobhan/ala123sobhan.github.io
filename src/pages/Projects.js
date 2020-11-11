7220b6597829cc0423abc2861247f5d750e9682b
Ala123Sobhan <ala123sobhan@gmail.com>
2020-11-11 10:36:58 -0500

    adding card details

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Particles from "react-tsparticles";
-import {Tabs, Tab} from "react-mdl"
+import minesweeper from "./minesweeper.png"
+import {Card, CardTitle, CardText, CardActions,Button, CardMenu, IconButton, Tabs, Tab, Grid, Cell} from "react-mdl"
 class Projects extends Component {
     constructor(props) {
         super(props)
@@ -9,7 +9,44 @@ class Projects extends Component {
           activeTab: 0   
         }
     }
-    
+    changeTabs=()=>{
+        if(this.state.activeTab === 0)
+        {
+            return(<div>
+               <Card shadow={5} style={{minWidth:"450", margin:"auto"}}>
+                   <CardTitle style={{color:"#ffff", height:"176px", background:'url(https://stackify.com/wp-content/uploads/2018/09/Java-Debugging-Tips-1280x720.jpg) center / cover'}}>
+                    Java Project #1
+                   </CardTitle>
+                   <CardText>
+                   Used Java and Graphical User Interface to make a traditional Minesweeper game where the user can select the dimensions of
+                  the grid and number of bombs. The objective of the game is that the player has to survive the game by not revealing all the
+                  cells containing bombs.
+                   </CardText>
+                <CardActions>
+                    <Button colored><a style={{textDecoration:"none", color:"royalblue", padding:"20px"}} href="https://github.com/Ala123Sobhan/MineSweeper" rel="noopener noreferrer" target="_blank">Github</a></Button>
+                    
+                    <Button colored><a style={{textDecoration:"none", color:"royalblue", padding:"30px"}} href="https://www.youtube.com/watch?v=CS5sN_Az9sI&t=649s" rel="noopener noreferrer" target="_blank">Demo</a></Button>
+                 </CardActions>
+                 <CardMenu style={{color:"#fff"}}>
+                   <IconButton name="share"/>
+                 </CardMenu>
+               </Card>
+              
+            </div>)
+        }
+        else if(this.state.activeTab === 1)
+        {
+            return(<div>
+                In Javascript
+            </div>)
+        }
+        if(this.state.activeTab === 2)
+        {
+            return(<div>
+                In React
+            </div>)
+        }
+    }
     render() {
         return (
             <div>    
@@ -20,9 +57,18 @@ class Projects extends Component {
               <Tab>React</Tab>
              </Tabs>
 
+        
          </div>
-         
-
+         <div className="projects-grid">
+             <Grid>
+                 <Cell col ={12}>
+                  <div className="content">
+                  {this.changeTabs()}
+                  </div>
+                 </Cell>
+             </Grid>
+            
+         </div>
         
         </div>
         )
