ab2a8194f27607e50c1660e11586d025aaa7229b
Ala123Sobhan <ala123sobhan@gmail.com>
2020-11-18 19:12:56 -0500

    Updates

---
+++ precache-manifest.04592473995573fef40bcf7334b2cd3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e975f3582e506d47e256d6a812bfd6fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd599e3381f6e8beb30a",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "21ec75f42a57573489a3",
+    "url": "/static/css/main.f1f22852.chunk.css"
+  },
+  {
+    "revision": "cd599e3381f6e8beb30a",
+    "url": "/static/js/2.608f7819.chunk.js"
+  },
+  {
+    "revision": "7ede0d87f7c6774aba504f63ace04000",
+    "url": "/static/js/2.608f7819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ec75f42a57573489a3",
+    "url": "/static/js/main.a941effa.chunk.js"
+  },
+  {
+    "revision": "cf294f91e0e148df4f84",
+    "url": "/static/js/runtime-main.ebad6d40.js"
+  },
+  {
+    "revision": "a973dc6c8a748a4e55120f2bc85db5ab",
+    "url": "/static/media/AlaImg.a973dc6c.png"
+  },
+  {
+    "revision": "ad78a2e82534429ff815c9bbca045ca1",
+    "url": "/static/media/Resume.ad78a2e8.pdf"
+  }
+]);
